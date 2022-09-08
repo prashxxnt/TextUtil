dfa51faf3f08a139be2cbdd87ced7814c1fb2074
prashxxnt <83673234+prashxxnt@users.noreply.github.com>
2022-09-09 00:57:28 +0530

    added about page with router

--- src/App.js
+++ src/App.js
@@ -5,6 +5,14 @@ import TextForm from './COMPNENTS/TextForm';
 import About from './COMPNENTS/About';
 import Alert from './COMPNENTS/Alert';
 import React, { useState } from 'react'
+
+import { render } from "react-dom";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+} from "react-router-dom";
+
 function App() {
   const [alert, setAlert]=useState(null);
 
@@ -19,14 +27,35 @@ function App() {
   }
   return (
     <>
+      <BrowserRouter>
       <Navbar title="textutils" about="about" />
       <Alert alert ={alert}/>
-      <div className="container  my-3">
-        <TextForm showAlert={showAlert} heading="ENTER THE HEADING" />
+      <div className="container">
+      <Routes>
+        <Route path="/" element={ <TextForm showAlert={showAlert} heading="ENTER THE HEADING" />}  />
+        <Route path="/About" element={<About/>} />
+      </Routes>
+
+      </div>
+    </BrowserRouter>
+
+      {/* <Navbar title="textutils" about="about" />
+      <Alert alert ={alert}/> */}
+      {/* <div className="container  my-3">
+        <Routes>
+          <Route path="/about">
+            <about/>
+          </Route>
+          <Route path ="/">
+            <TextForm showAlert={showAlert} heading="ENTER THE HEADING" />
+          </Route> */}
+
+
+        {/* </Routes> */}
+        {/* <TextForm showAlert={showAlert} heading="ENTER THE HEADING" /> */}
 
         {/* <About/> */}
-      </div>
-
+      {/* </div> */}
     </>
   );
 }
